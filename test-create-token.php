6b493de150f07253b2f8d5a1e20ac89a6a41a6b7
Jeff Bouchard <23242431+Jeff-Bouchard@users.noreply.github.com>
2024-10-12 15:53:47 -0400

    Update test-create-token.php
    typo

--- test-create-token.php
+++ test-create-token.php
@@ -14,9 +14,9 @@ if ($argc == 2) {
             echo "OK";
         } catch (\Throwable $th) {
             if (false !== strpos($th->getMessage(), 'there are 1 pending operations on that name')) {
-                echo "Token with addr:$ness_addr olready created and the operation is currently pending";
+                echo "Token with addr:$ness_addr is already created or the operation is currently pending";
             } elseif (false !== strpos($th->getMessage(), 'name_new on an unexpired name')) {
-                echo "Token with addr:$ness_addr olready exist";
+                echo "Token with addr:$ness_addr already exist";
             } else {
                 echo "Error: " . $th->getMessage();
             }
